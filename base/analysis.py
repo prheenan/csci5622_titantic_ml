fixed SVM parameters and masking. TODO, in this order (1) add in functionality to re-iterate through best trial validation of 0. (2) add in funcitonality to lower numbers of parameters by beta. this is essentially just maybe a call-back in the mask? (3) get back in label bar, or add to title. (4) add in like a quad-gram feature, use NOT(hasSeocndName) AND NOTE(hasCabin) as mask, should give about half the survivors. ticket prefix sucks as a feature because it happens so rarely.

base/analysis.py
@@ -86,6 +86,8 @@ def sortByPred(matrix,yPred,yActual):
     badIdx,predDeath,predSurv = getIdxMistakes(yPred,yActual)
     score = [ 1 if i in predDeath else 0 for i in range(matrix.shape[0])]
     sortIdx = np.argsort(score)
+    # we now have the array like [1,2,...,len(predDeath),...,len(N)]
+    # sort *within* in the dead and the survivors
     return matrix[sortIdx,:]
 
 def profileLosers(saveDir,label,yPred,yActual,rawDat,dataClass,featureMat,
@@ -105,11 +107,14 @@ def profileLosers(saveDir,label,yPred,yActual,rawDat,dataClass,featureMat,
                 label="Divides {:d} actual deceased from {:d} actual survived".\
                 format(nSurv,nDead))
     plt.legend(loc="upper right", bbox_to_anchor=(0.4, -0.4))
+    badVals = rawDat[badIdx,:]
+    np.savetxt(saveDir + 'debug_{:s}.csv'.format(label),badVals,fmt="%s",
+               delimiter=',')
     pPlotUtil.savefig(fig,saveDir + "mOut" + label,tight=True)
 
 
 def predict(fitter,x,yReal,rawDat,label,saveDir,colNames,fitterCoeff,objClass,
-            featureObjects,saveBad=False,saveCoeffs=False,plot=True):
+            featureObjects,saveBad=True,saveCoeffs=False,plot=True):
     try:
         yPred = fitter.predict(x)
     except TypeError:

mainLogReg.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression
 
 
 def defaultFitterParams():
-    return np.logspace(-3,3,20)
+    return np.logspace(-3,3,1)
 
 def defaultFitter(iterNum):
     nEst = defaultFitterParams()[iterNum]
@@ -19,7 +19,7 @@ label='logreg'
 forceRun = True # otherwise, use checkpoint (cached file)
 fullOutput = "./work/out/"+label+"/"
 valid = 0.1
-nTrials = int(np.ceil(1./valid))
+nTrials =1# int(np.ceil(1./valid))
 mean,std= main.run(defaultFitter,defaultFitterParams,
                    defaultCoeff,lrClass,label=label,valid=0.1,nTrials=nTrials,
                    force=forceRun,plot=False,profile=False)